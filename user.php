84113a70377a9aa9a3096d68ad98da53a6d91da3
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2020-05-19 17:01:40 +0700

    * Modify user.php

--- user.php
+++ user.php
@@ -6,7 +6,7 @@
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
                         <h4 class="page-title">Dashboard</h4>
                     </div>
-                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                    <div class="col-lg-3 col-sm-8 col-md-8 col-xs-12">
                     </div>
                     <!-- /.col-lg-12 -->
                 </div>
