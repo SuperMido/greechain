78066194b173dd80fe6be7fd1d17e9bfd844f9db
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2020-05-21 14:14:36 +0700

    + Login guideline| * Products comments posting section

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include('templates/_header.php'); ?>
+
 <body>
     <!-- Preloader -->
     <div class="preloader">
@@ -7,32 +8,135 @@
     <section id="wrapper" class="login-register">
         <div class="login-box">
             <div class="login-white-box">
-                    <div class="form-group text-center">
-                        <div class="col-xs-12">
-                             <!-- <img src="dist/images/imperialsofetch.png"/> -->
-                             <img src="plugins/images/logogre.png" style="width: 225px; height: 225px;" />
-                        </div>
+                <div class="form-group text-center">
+                    <div class="col-xs-12">
+                        <!-- <img src="dist/images/imperialsofetch.png"/> -->
+                        <img src="plugins/images/logogre.png" style="width: 225px; height: 225px;" />
                     </div>
-                    <div class="form-group  text-center">
-                        <div class="col-xs-12">
-                           <h3><b>Chào mừng đến với công nghệ quản lý cà phê hoàn toàn mới !</b></h3>
-                            <h5>Hệ thống website áp dụng kỹ thuật blockchain vào quy trình sản xuất Cà Phê</h5>
-                        </div>
+                </div>
+                <div class="form-group  text-center">
+                    <div class="col-xs-12">
+                        <h3><b>Chào mừng đến với công nghệ quản lý cà phê hoàn toàn mới !</b></h3>
+                        <p><strong>Hệ thống website áp dụng kỹ thuật blockchain vào quy trình sản xuất Cà Phê</strong></p>
                     </div>
-                    <div class="form-group text-center">
-                        <div class="col-xs-6 p-b-20">
-                            <a href="https://metamask.io/" target="_blank" class="btn btn-rounded btn-block btn-success">Tải Metamask</a>
-                        </div>
-                         <div class="col-xs-6 p-b-20">
-                            <a href="admin.php" class="btn btn-rounded btn-block btn-danger">Đăng nhập với quyền quản trị</a>
-                        </div>
-                        <div class="col-xs-6 p-b-20">
-                            <a href="user.php" class="btn btn-rounded btn-block btn-info">Đăng nhập với quyền người dùng</a>
-                        </div>
-                    </div>   
+                </div>
+                <div class="form-group text-center">
+                    <div class="col-xs-6 p-b-20">
+                        <a href="https://metamask.io/" target="_blank" class="btn btn-rounded btn-block btn-success"><strong>Tải Metamask</strong></a>
+                    </div>
+                    <div class="col-xs-6 p-b-20">
+                        <a href="admin.php" class="btn btn-rounded btn-block btn-danger"><strong>Đăng nhập với quyền quản trị</strong></a>
+                    </div>
+                    <div class="col-xs-6 p-b-20">
+                        <a href="user.php" class="btn btn-rounded btn-block btn-info"><strong>Đăng nhập với quyền thành viên</strong></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="info-white-box">
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <ul class="timeline">
+                            <li>
+                                <div class="timeline-badge success">
+                                    <i class="fa fa-arrow-down"></i>
+                                </div>
+                                <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                        <h4 class="timeline-title">Bước 1:</h4>
+                                        <p><small class="text-muted text-success activityDateTime"></small> </p>
+                                        <span class="activityQrCode"></span>
+                                    </div>
+                                    <div class="timeline-body">
+                                        <table class="table activityData table-responsive">
+                                            <tr>
+                                                <td colspan="2">
+                                                    <p><strong>Tải xuống <a href="https://metamask.io" target="_blank">Metamask</a></strong></p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="timeline-inverted">
+                                <div class="timeline-badge success">
+                                    <i class="fa fa-arrow-down"></i>
+                                </div>
+                                <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                        <h4 class="timeline-title">Bước 2</h4>
+                                        <p><small class="text-muted text-success activityDateTime"></small> </p>
+                                        <span class="activityQrCode"></span>
+                                    </div>
+                                    <div class="timeline-body">
+                                        <table class="table activityData table-responsive">
+                                            <tr>
+                                                <td colspan="2">
+                                                    <p><strong>Tạo tài khoản Metamask của doanh nghiệp với Private Key:</strong></p>
+                                                    <h4>abcxyz</h4>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="timeline-badge success">
+                                    <i class="fa fa-arrow-down"></i>
+                                </div>
+                                <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                        <h4 class="timeline-title">Bước 3</h4>
+                                        <p><small class="text-muted text-success activityDateTime"></small> </p>
+                                        <span class="activityQrCode"></span>
+                                    </div>
+                                    <div class="timeline-body">
+                                        <table class="table activityData table-responsive">
+                                            <tr>
+                                                <td colspan="2">
+                                                    <p><strong>Tạo tài khoản Metamask của thành viên với Private Key:</strong></p>
+                                                    <h4>abcxyz</h4>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="timeline-inverted">
+                                <div class="timeline-badge success">
+                                    <i class="fa fa-arrow-down"></i>
+                                </div>
+                                <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                        <h4 class="timeline-title">Bước 4</h4>
+                                        <p><small class="text-muted text-success activityDateTime"></small> </p>
+                                        <span class="activityQrCode"></span>
+                                    </div>
+                                    <div class="timeline-body">
+                                        <table class="table activityData table-responsive"  text-align="center">
+                                            <tr>
+                                                <td colspan="2">
+                                                    <p><strong>Chọn "Đăng nhập với quyền quản trị"</strong></p>
+                                                    <h4>hoặc</h4>
+                                                    <p><strong>Chọn "Đăng nhập với quyền thành viên"</strong></p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </li>
+                            
+                        </ul>
+
+                    </div>
+                </div>
             </div>
         </div>
     </section>
+
+
     <!-- jQuery -->
     <script src="plugins/bower_components/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap Core JavaScript -->
@@ -51,4 +155,3 @@
 </body>
 
 </html>
-
